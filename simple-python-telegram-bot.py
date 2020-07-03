362f44323e16a2d6505ac6dd0c2b56705ec4834e
Sergio <babik.sergey@gmail.com>
2020-07-03 17:38:23 +0300

    create simply telegram bot

---
+++ simple-python-telegram-bot.py
@@ -0,0 +1,9 @@
+import telebot
+
+bot = telebot.TeleBot("1258885982:AAHoO--kqaVSloky2EEa1QpvHGfdBciJ-Fc")
+
+@bot.message_handler(content_types=['text'])
+def send_echo(message):
+	bot.send_message(message.chat.id, message.text)
+
+bot.polling( none_stop = True )
